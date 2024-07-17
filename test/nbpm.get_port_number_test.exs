e40b04d1befd4842d7baa617b3a03a0ff96ab5a9
Dmitriy Pertsev <davaeron@gmail.com>
2024-07-17 11:27:19 +0300

    refactor

--- test/nbpm.get_port_number_test.exs
+++ test/nbpm.get_port_number_test.exs
@@ -4,18 +4,27 @@ defmodule Mix.Tasks.Nbpm.GetPortNumberTest do
   import ExUnit.CaptureIO
   import NbpmTest.Support
 
-  defp assert_node_to_listen_port({input, expected}) do
+  defp assert_cmd_arg_to_listen_port({input, expected}) do
     assert "#{expected}\n" ==
              capture_io(fn -> Mix.Task.rerun("nbpm.get_port_number", [input]) end)
   end
 
+  defp assert_app_name_to_listen_port({input, expected}) do
+    change_app_in_mix_project(input)
+    assert "#{expected}\n" == capture_io(fn -> Mix.Task.rerun("nbpm.get_port_number", []) end)
+  end
+
+  defp change_app_in_mix_project(app_name) do
+
+  end
+
   describe "test get port number" do
-    test "get listen port by arg" do
-      Enum.each(nodes_ports(), &assert_node_to_listen_port/1)
+    test "get listen port by command line argument" do
+      Enum.each(names_to_ports(), &assert_cmd_arg_to_listen_port/1)
     end
 
-    test "get listen port by project app" do
-      assert "2937\n" == capture_io(fn -> Mix.Task.rerun("nbpm.get_port_number", []) end)
+    test "get listen port by project app name" do
+      Enum.each(names_to_ports(), &assert_app_name_to_listen_port/1)
     end
   end
 end
